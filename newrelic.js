de70f28f18f565a5eb2f9ea0aa2c534b66bbb1c3
Mark Hayford <mhayford29@gmail.com>
2019-06-17 12:40:11 -0700

    fixed somethin

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: baa01d48dda48885b7961763faf6ad18b392c066,
+  license_key: '',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
