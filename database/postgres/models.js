eab8cd0367fe5fd0282d2000df996ed9fd3c60ac
Mark Hayford <mhayford29@gmail.com>
2019-07-23 10:47:35 -0700

    uncommented schema

--- database/postgres/models.js
+++ database/postgres/models.js
@@ -3,24 +3,24 @@
 //UNCOMMENT BELOW FOR SEQUELIZE
 //********************************************************************** 
 
-// const Sequelize = require('sequelize');
-// const db = require('./index');
+const Sequelize = require('sequelize');
+const db = require('./index');
 
-// const Product = db.define('products', {
-//   product_name: { type: Sequelize.STRING },
-//   designer: { type: Sequelize.STRING },
-//   price: { type: Sequelize.FLOAT },
-//   stars: { type: Sequelize.FLOAT },
-//   reviews: { type: Sequelize.INTEGER },
-//   description: { type: Sequelize.STRING },
-//   fit: { type: Sequelize.STRING },
-//   sizes: { type: Sequelize.STRING },
-//   colors: { type: Sequelize.STRING },
-//   image_urls_color1: { type: Sequelize.STRING(2000) },
-//   image_urls_color2: { type: Sequelize.STRING(2000) },
-// })
+const Product = db.define('products', {
+  product_name: { type: Sequelize.STRING },
+  designer: { type: Sequelize.STRING },
+  price: { type: Sequelize.FLOAT },
+  stars: { type: Sequelize.FLOAT },
+  reviews: { type: Sequelize.INTEGER },
+  description: { type: Sequelize.STRING },
+  fit: { type: Sequelize.STRING },
+  sizes: { type: Sequelize.STRING },
+  colors: { type: Sequelize.STRING },
+  image_urls_color1: { type: Sequelize.STRING(2000) },
+  image_urls_color2: { type: Sequelize.STRING(2000) },
+})
 
-// db.sync();
+db.sync();
 
-// module.exports = Product;
+module.exports = Product;
 
